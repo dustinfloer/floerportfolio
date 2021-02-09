0495d229c94241cfe0f24c7b6c406cd21384d7b2
Dustin Floer <dustinfloer@dustins-air.lan>
2021-02-09 11:53:16 -0600

    javascript updates

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    navbarFixedTopAnimation();
+  });
+
+  document.addEventListener("DOMContentLoaded", function (event) {
+    navActivePage();
+    scrollRevelation('.reveal');
+  });
